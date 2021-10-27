a8558324d8a432117528a53ea29cf22ae1e9341b
Ernesto Camacho <ernesto.camachoa9805gmail.com>
2021-10-27 09:44:00 -0500

    main page - city select

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import {CitySelect, MainPage} from './pages';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/citySelect">
+          <CitySelect/>
+        </Route>
+
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
