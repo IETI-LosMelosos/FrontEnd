2f37feaa8d3897782b474bd7c990d3dd6562e4a3
DrLolo03 <lorenzomarquezpinto@gmail.com>
2021-11-04 15:03:45 -0500

    About fix commit

---
+++ src/components/CustomBtnAboutUs.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Button} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles'
+
+const StyledButton = withStyles({
+    root: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "45px",
+        padding: "0 25px",
+        boxSizing: "border-box",
+        borderRadius: 0, 
+        background: "#7FFF00",
+        color: "#696969",
+        transform: "none",
+        boxShadow: "6px 6px 0 0 #c7d8ed",
+        transition: "background .3s,border-color .3s,color .3s",
+        "&:hover": {
+            backgroundColor:  "#006400"
+          },
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
+
+  export function CustomBtnAboutUs(props) {
+    return (
+        <StyledButton variant="contained">{props.txt}</StyledButton>
+    )
+}
+
+
+
